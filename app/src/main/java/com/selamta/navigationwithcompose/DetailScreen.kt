e133f1ebdc9b5e01ef1369d95bf205456602d7f9
Kal <milkiyasmicha033@gmail.com>
2023-11-09 17:07:55 +0300

    change font size and color

--- app/src/main/java/com/selamta/navigationwithcompose/DetailScreen.kt
+++ app/src/main/java/com/selamta/navigationwithcompose/DetailScreen.kt
@@ -1,8 +1,6 @@
 package com.selamta.navigationwithcompose
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -11,10 +9,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 
 @Composable
 fun DetailScreen(navController: NavController) {
@@ -26,8 +22,8 @@ fun DetailScreen(navController: NavController) {
     ) {
         Text(
             text = "This is Detail Screen",
-            color = MaterialTheme.colorScheme.primary,
-            fontSize = MaterialTheme.typography.headlineMedium.fontSize,
+            color = MaterialTheme.colorScheme.tertiary,
+            fontSize = MaterialTheme.typography.headlineLarge.fontSize,
             fontWeight = FontWeight.Bold
         )
         Button(onClick = {
