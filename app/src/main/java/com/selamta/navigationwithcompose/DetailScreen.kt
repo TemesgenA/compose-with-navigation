51585a284dc3323c752217c6cd36ca670140eeee
Temesgen Ayneka <temesgen.ayneka@gmail.com>
2023-11-09 22:56:57 +0300

    modify text alignment

--- app/src/main/java/com/selamta/navigationwithcompose/DetailScreen.kt
+++ app/src/main/java/com/selamta/navigationwithcompose/DetailScreen.kt
@@ -1,8 +1,11 @@
 package com.selamta.navigationwithcompose
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -10,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -33,11 +38,15 @@ fun DetailScreen(navController: NavController) {
 
         }
 
+        Spacer(modifier = Modifier.height(16.dp));
+
         Text(
             text = "This is simple implementation intended to show navigation in compose",
+            textAlign = TextAlign.Center,
             color = MaterialTheme.colorScheme.tertiary,
             fontSize = MaterialTheme.typography.bodyMedium.fontSize,
             fontWeight = FontWeight.Bold
         )
+
     }
 }
